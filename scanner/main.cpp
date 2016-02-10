06b871e16932925241fdc3d8d5bc8b1ec7adb516
skyclad0x7b7 <skyclad0x7b7@gmail.com>
2016-02-11 01:45:33 +0900

    Packet capture, classify, and insert Mysql table log.

--- scanner/main.cpp
+++ scanner/main.cpp
@@ -11,7 +11,6 @@
 #include <iostream>
 #include <mysql/mysql.h>
 
-
 using namespace std;
 
 #define BUFSIZE 65536
@@ -46,29 +45,53 @@ int main(int argc, char *argv[])
 	MYSQL pMysql;
     	MYSQL_RES *res;
     	MYSQL_FIELD *field;
-    	MYSQL_ROW row;
-    	unsigned int field_num;
-    	char buf[SQL_BUFSIZE] = {0};
+	MYSQL_ROW row;
+	char query[SQL_BUFSIZE];
+    	unsigned int field_num, row_num, tmp;
+    	char sql_buf[SQL_BUFSIZE] = "SELECT mac_addr FROM `user`";
     	mysql_init(&pMysql);
     	mysql_options(&pMysql, MYSQL_READ_DEFAULT_GROUP, "my_prog_name");
-    	if(!mysql_real_connect(&pMysql, "localhost", "ID", "PASSWD", "wdds_db", 3306, NULL, 0)){
+    	
+	char **mac_list;
+
+	if(!mysql_real_connect(&pMysql, "localhost", "ID", "PASSWD", "wdds_db", 3306, NULL, 0)){
     	    fprintf(stderr, "[*] Failed to Connect to the database : %s\n", mysql_error(&pMysql));
+		return 1;
     	}
 
+	if(mysql_real_query(&pMysql, sql_buf, strlen(sql_buf))){ 
+                printf("[*] An Error occured in mysql_real_query : %s\n", mysql_error(&pMysql));  // Error Occured
+               	return 1;
+        }
 
+	res = mysql_store_result(&pMysql);
+	field_num = mysql_num_fields(res);
+	row_num = mysql_num_rows(res);
+
+	mac_list = (char **)malloc(row_num * sizeof(char *));
+	
+	cout << "[*] Saved MAC Address" << endl;
+	cout << "Field_num : " << field_num << endl;
+	tmp = 0;
+	while((row = mysql_fetch_row(res))){                    // print rows
+        	printf("|%17s|\n", row[0] ? row[0] : "NULL");
+		mac_list[tmp] = (char *)malloc(18);
+		strcpy(mac_list[tmp++], row[0] ? row[0] : "NULL");
+        }
 	char errBuf[256];
 	const u_char *data;
 	pcap_pkthdr *pkthdr;
 	BF_header bf_header;
 	pcap_t *handle = pcap_open_live(argv[1], BUFSIZE, 1, 10, errBuf);
-    	
+	char src[18], dest[18];    	
+
 	if(handle == NULL){
         	fprintf(stderr, "Couldn't open device %s : %s\n",  argv[1], errBuf);
         	return 1;
    	}
-
+	cout << "[*] Please input any key to start scanning" << endl;
+	getchar();
 	while(true) {	
-
 		int res = pcap_next_ex(handle, &pkthdr, &data);
 
 		if(res == 0) continue;
@@ -77,10 +100,20 @@ int main(int argc, char *argv[])
 		data += *(u_int16_t *)(data+2); // Skip Radiotap Header
 		memcpy(&bf_header, data, 0x18);
 		if(bf_header.frame_control != 0x0050) continue; // Probe response only
-		fprintf(stdout, "BSS : %02X:%02X:%02X:%02X:%02X:%02X    ", bf_header.bss[0], bf_header.bss[1], bf_header.bss[2], bf_header.bss[3], bf_header.bss[4], bf_header.bss[5]);
-		fprintf(stdout, "SRC : %02X:%02X:%02X:%02X:%02X:%02X    ", bf_header.src[0], bf_header.src[1], bf_header.src[2], bf_header.src[3], bf_header.src[4], bf_header.src[5]);
-		fprintf(stdout, "DEST : %02X:%02X:%02X:%02X:%02X:%02X\n", bf_header.dest[0], bf_header.dest[1], bf_header.dest[2], bf_header.dest[3], bf_header.dest[4], bf_header.dest[5]);
+		sprintf(src, "%02X:%02X:%02X:%02X:%02X:%02X", bf_header.src[0], bf_header.src[1], bf_header.src[2], bf_header.src[3], bf_header.src[4], bf_header.src[5]);
+		sprintf(dest, "%02X:%02X:%02X:%02X:%02X:%02X", bf_header.dest[0], bf_header.dest[1], bf_header.dest[2], bf_header.dest[3], bf_header.dest[4], bf_header.dest[5]);
+		cout << "SRC : " << src << "    DEST : " << dest << endl;
+		for(int i=0; i<row_num ; i++) if( !strcmp(mac_list[i], src) || !strcmp(mac_list[i], dest) ) {
+				sprintf(query, "INSERT INTO `log` (mac_addr) VALUES ('%s')", mac_list[i]);
+				if(mysql_real_query(&pMysql, query, strlen(query))){
+         			       printf("[*] An Error occured in mysql_real_query : %s\n", mysql_error(&pMysql));  // Error Occured
+         			       return 1;
+        			}
+				cout << "[*] Query injected : " << query << endl;
+			}
 	}
 	
+	for(int i=0; i<field_num; i++) free(row[i]);
+	free(mac_list);
 	return 0;
 }
