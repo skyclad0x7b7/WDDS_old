c8ef00e4c02401da428b4b134a151fc9c5b9f205
skyclad0x7b7 <skyclad0x7b7@gmail.com>
2016-02-14 01:50:04 +0900

    Crash

--- scanner/main.cpp
+++ scanner/main.cpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <algorithm>
 #include <unistd.h>
+#include <time.h>
 
 using namespace std;
 
@@ -41,6 +42,9 @@ typedef struct ieee80211_wlan_management {
 
 bool isContain(std::vector<string>*, string);
 bool insertQuery(MYSQL *, char *query);
+void *timer(void *);
+void dataLogging(MYSQL *pMysql);
+
 
 int main(int argc, char *argv[])
 {
@@ -60,6 +64,8 @@ int main(int argc, char *argv[])
     	mysql_options(&pMysql, MYSQL_READ_DEFAULT_GROUP, "my_prog_name");
     	int flag = false;
 	std::vector<string> mac_list;
+
+	pthread_t timerThread;
 
 	if(!mysql_real_connect(&pMysql, "localhost", "ID", "PASSWD", "wdds_db", 3306, NULL, 0)){
     	    fprintf(stderr, "[*] Failed to Connect to the database : %s\n", mysql_error(&pMysql));
@@ -92,6 +98,11 @@ int main(int argc, char *argv[])
    	}
 	cout << "[*] Please input any key to start scanning" << endl;
 	getchar();
+
+	if(pthread_create(&timerThread, NULL, timer, (void *)&pMysql)) {
+		cout << "Threading Error!!" << endl;
+		return 1;
+	};
 	while(true) {	
 		int res = pcap_next_ex(handle, &pkthdr, &data);
 
@@ -104,25 +115,10 @@ int main(int argc, char *argv[])
 		sprintf(src, "%02X:%02X:%02X:%02X:%02X:%02X", bf_header.src[0], bf_header.src[1], bf_header.src[2], bf_header.src[3], bf_header.src[4], bf_header.src[5]);
 		sprintf(dest, "%02X:%02X:%02X:%02X:%02X:%02X", bf_header.dest[0], bf_header.dest[1], bf_header.dest[2], bf_header.dest[3], bf_header.dest[4], bf_header.dest[5]);
 		cout << "SRC : " << src << "    DEST : " << dest << endl;
-		if(!isContain(&mac_list, std::string(src))) {
-			sprintf(query, "INSERT INTO `user` (name, mac_addr) VALUES ('Unknown', '%s')", src);
-			insertQuery(&pMysql, query);
-			mac_list.push_back(src);
-			cout << "Pushed" << endl;
-		} else {
-                        sprintf(query, "INSERT INTO `log` (mac_addr) VALUES ('%s')", src);
-                	insertQuery(&pMysql, query);
-		}
-
-		if(!isContain(&mac_list, std::string(dest))) {
-                        sprintf(query, "INSERT INTO `user` (name, mac_addr) VALUES ('Unknown', '%s')", dest);
-			insertQuery(&pMysql, query);
-			mac_list.push_back(dest);
-			cout << "Pushed" << endl;
-                } else {
-                        sprintf(query, "INSERT INTO `log` (mac_addr) VALUES ('%s')", dest);
-                	insertQuery(&pMysql, query);
-		}									// @ ToDo : MySQL Query function & etc...
+                sprintf(query, "INSERT INTO `temp_log` (mac_addr) VALUES ('%s')", src);
+                insertQuery(&pMysql, query);
+		sprintf(query, "INSERT INTO `temp_log` (mac_addr) VALUES ('%s')", dest);
+                insertQuery(&pMysql, query);
 
 		/*flag = false;
 		for(int i=0; i<row_num ; i++) if( !strcmp(mac_list[i], src) || !strcmp(mac_list[i], dest) ) {
@@ -143,7 +139,8 @@ int main(int argc, char *argv[])
 }
 
 
-bool isContain(std::vector<string> *mac_list, std::string comp) {
+bool isContain(std::vector<string> *mac_list, std::string comp) 
+{
 	if( std::find(mac_list->begin(), mac_list->end(), comp) != mac_list->end() ) return true;
 	return false;
 }
@@ -156,4 +153,34 @@ bool insertQuery(MYSQL *pMysql, char *query)
               	return false;
         }
 	return true;
+}
+
+void *timer(void *tmp)
+{
+	MYSQL pMysql = (MYSQL &)tmp;
+	unsigned int cur, next;
+	cur = (unsigned int)time(NULL);
+	while(true)
+	{
+		next = (unsigned int)time(NULL);
+		if((next - 10) == cur) {
+			dataLogging(&pMysql);
+		}
+	}	
+}
+
+void dataLogging(MYSQL *pMysql)
+{
+	MYSQL_RES *res;
+	MYSQL_FIELD *field;
+        MYSQL_ROW row;
+	char query[SQL_BUFSIZE];
+	insertQuery(pMysql, "SELECT DISTINCT(mac_addr) from temp_log");
+	res = mysql_store_result(pMysql);
+	int field_num = mysql_num_fields(res);
+        int row_num = mysql_num_rows(res);
+	while((row = mysql_fetch_row(res))){
+		sprintf(query, "INSERT INTO `log` (mac_addr) values ('%s')", row[0]);
+		insertQuery(pMysql, query);
+        }
 }
